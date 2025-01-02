71c2491e9c832e1bb3a768111a660f971f5d83f4
Hantao Cui <cuihantao@gmail.com>
2025-01-02 17:27:56 -0500

    Allow "Present" as a valid endTime

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 
 export function reformatDate(dateStr, toFormat, fromFormat = 'YYYY-MM-DD') {
+  if (dateStr === 'Present') return 'Present';
   return dayjs(dateStr).format(toFormat)
 }
